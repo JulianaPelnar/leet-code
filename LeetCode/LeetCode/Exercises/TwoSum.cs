40f314604448159d46c5755297b27cb47ce30036
Juliana Pelnar <julianapelnar@gmail.com>
2021-09-08 23:06:56 -0300

    First exercise, TwoSum

---
+++ LeetCode/LeetCode/Exercises/TwoSum.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Exercises
+{
+    /*
+     Exercise:
+      Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+      You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+      You can return the answer in any order.
+
+     Result:
+      Runtime: 384 ms, faster than 32.35% of C# online submissions for Two Sum.
+      Memory Usage: 32.5 MB, less than 66.11% of C# online submissions for Two Sum.
+     */
+    public class TwoSum
+    {
+        public TwoSum() { }
+        public int[] Solution(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Count(); i++)
+            {
+                if (dic.ContainsKey(target - nums[i]))
+                {
+                    result[1] = i;
+                    result[0] = dic.GetValueOrDefault(target - nums[i]);
+                    return result;
+                }
+                dic.TryAdd(nums[i], i);
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Exercises/TwoSumTest.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Exercises;
+using Xunit;
+
+namespace LeetCodeTests.Exercises
+{
+    public class TwoSumTest
+    {
+        public TwoSum twoSum = new TwoSum();
+
+        [Fact]
+        public void Test1()
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            var target = 9;
+            int[] response = twoSum.Solution(nums, target);
+            int[] expectedResponse = new int[] { 0, 1 };
+            Assert.Equal(response, expectedResponse);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] nums = { 3, 2, 4 };
+            var target = 6;
+            int[] response = twoSum.Solution(nums, target);
+            int[] expectedResponse = new int[] { 1, 2 };
+            Assert.Equal(response, expectedResponse);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] nums = { 3, 3 };
+            var target = 6;
+            int[] response = twoSum.Solution(nums, target);
+            int[] expectedResponse = new int[] { 0, 1 };
+            Assert.Equal(response, expectedResponse);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int[] nums = { 3, 2, 3 };
+            var target = 6;
+            int[] response = twoSum.Solution(nums, target);
+            int[] expectedResponse = new int[] { 0, 2 };
+            Assert.Equal(response, expectedResponse);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            int[] nums = { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+            var target = 11;
+            int[] response = twoSum.Solution(nums, target);
+            int[] expectedResponse = new int[] { 5, 11 };
+            Assert.Equal(response, expectedResponse);
+        }
+    }
+}
